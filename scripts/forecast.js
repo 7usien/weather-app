3d68597fca136071dd41a0450fc171da1410ed9c
7usien <husienadel2022@gmail.com>
2022-01-18 13:18:46 +0300

    update api with https

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -2,7 +2,8 @@ const key = '3NYrsGmRCkO8Y8OBTdAtJMMAqB5zDr5A';
 
 // GET CITY INFO >> KEY
 const getCity = async (city) => {
-	const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+	const base =
+		'https://dataservice.accuweather.com/locations/v1/cities/search';
 	const query = `?apikey=${key}&q=${city}`;
 	const response = await fetch(base + query);
 
@@ -19,7 +20,7 @@ const getCity = async (city) => {
 
 /// GET WEATHER INFO
 const getWeather = async (id) => {
-	const base = `http://dataservice.accuweather.com/currentconditions/v1/`;
+	const base = `https://dataservice.accuweather.com/currentconditions/v1/`;
 
 	const query = `${id}?apikey=${key}`;
 	const response = await fetch(base + query);
